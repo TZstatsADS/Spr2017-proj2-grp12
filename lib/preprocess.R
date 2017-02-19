eb9c8d86ee523ff55a534b43038eac3f1f407854
Ziwei Meng <Zoe@dyn-160-39-9-79.dyn.columbia.edu>
2017-02-18 20:23:37 -0500

    add newest_data.csv and analysis.twb

--- lib/preprocess.R
+++ lib/preprocess.R
@@ -16,9 +16,9 @@ shrink.data <- function(i,fnames){
   target <- fread(fnames[i])
   target <- tbl_df(target)
   #select columns with <0.25 missing rate conditional on rows with non-null admission rate
-  target <- target%>%filter(ADM_RATE!="NULL")%>%select_(.dots = cols)
+  target <- target%>%filter((ADM_RATE!="NULL")&(ADM_RATE>0))%>%select_(.dots = cols)
   high.missing.cols <- names(target%>%select(which(colMeans(is.nullstr(.)) > 0.25)))
-  save_name <- paste('Spr2017-proj2-grp12/data/selected_',strsplit(fnames[i],'/')[[1]][2],sep = '')
+  save_name <- paste('Spr2017-proj2-grp12/output/selected_',strsplit(fnames[i],'/')[[1]][2],sep = '')
   write.csv(target,save_name)
   print(paste(c('Following columns have missing rate more than 0.25:',high.missing.cols)))
 }
@@ -29,8 +29,28 @@ for (i in 10:19){
   print(paste(str(i),'th file has been preprocessed.',sep = ''))
 }
 
+#popularity against value_added
+target <- fread('Spr2017-proj2-grp12/output/selected_MERGED2014_15_PP.csv')
+# compute popularity
+target$UGDS <- as.numeric(as.character(target$UGDS))
+target$ADM_RATE <- as.numeric(as.character(target$ADM_RATE))
+target$popularity <- target$UGDS/(target$ADM_RATE)
+# is.na(target$popularity) <- do.call(cbind,lapply(target$popularity, is.infinite))#replace Inf with NA
 
+# compute value_added
+ref_tgt <- fread('Spr2017-proj2-grp12/output/selected_MERGED2009_10_PP.csv')
+ref_tgt$MD_EARN_WNE_P6 <- as.numeric(as.character(ref_tgt$MD_EARN_WNE_P6)) 
+target$MD_FAMINC <- as.numeric(as.character(target$MD_FAMINC))
+new_df <- target%>%select(UNITID,MD_FAMINC)%>%inner_join((ref_tgt%>%select(UNITID,MD_EARN_WNE_P6)))
+new_df$value_added <- new_df$MD_EARN_WNE_P6/new_df$MD_FAMINC
+is.na(new_df$value_added) <- do.call(cbind,lapply(new_df$value_added, is.infinite))#replace Inf with NA
+target <- target%>%left_join((new_df%>%select(UNITID,value_added)))
+write.csv(target,'Spr2017-proj2-grp12/output/newest_data.csv')
 
+# compute new variable as is.major
+target <- fread('Spr2017-proj2-grp12/output/newest_data.csv')
+  
+  
 # null.to.na <- function(x) {
 #   if ((x=="NULL")) {
 #     result <- NA
